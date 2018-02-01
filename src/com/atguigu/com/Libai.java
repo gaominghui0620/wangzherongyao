31850dcb213ffaa8146bf6bd63265c44063b1abb
linxiaorui6666 <603526567@qq.com>
2018-02-01 15:02:23 +0800

    更新一

--- src/com/atguigu/com/Libai.java
+++ src/com/atguigu/com/Libai.java
@@ -4,6 +4,10 @@ public class Libai {
 
 	public static void main(String[] args) {
 		System.out.println("666666666666666");
+		System.out.println("555555555555555");
+		System.out.println("444444444444444");
+		System.out.println("333333333333333");
+		System.out.println("222222222222222");
 
 	}
 
